baeebe2e30b8a532e5974d58957640e4fd7322de
pyspice <roach.97p@gmail.com>
2023-02-24 20:24:45 +0300

    FIxed fetcher test

--- src/fetcher/fetch_test.cmd
+++ src/fetcher/fetch_test.cmd
@@ -1,1 +1,9 @@
-python fetch.py -f="test.txt" https://dnd.su/spells/205-fireball/ https://dnd.su/spells/464-chaos_bolt/ https://dnd.su/spells/408-whirlwind/
+python fetch.py -f="test.txt" ^
+    https://dnd.su/spells/16-mordenkainen_s_magnificent_mansion/ ^
+    https://dnd.su/spells/1922-gift_of_gab/ ^
+    https://dnd.su/spells/205-fireball/ ^
+    https://dnd.su/spells/19-dream/ ^
+    https://dnd.su/spells/214-time_stop/ ^
+    https://dnd.su/spells/264-foresight/ ^
+    https://dnd.su/spells/508-invulnerability/ ^
+    https://dnd.su/spells/125-true_resurrection/
